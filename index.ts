cfa7b43d074f5baf5e34d179b6e93bd50c58b393
Luca Tabone <taboneluca3@gmail.com>
2024-07-07 17:08:08 +0200

    chore: remove dev code

--- index.ts
+++ index.ts
@@ -1,14 +1,1 @@
-import fs from "fs/promises";
-import path from "path";
-import { FMG } from "./src";
-
-fs.readFile(path.join(__dirname, "fmg.map"))
-  .then((content) => {
-    const fmg = FMG.parse({
-      debug: true,
-      data: content.toString("utf-8"),
-    });
-
-    console.log(fmg.burgs);
-  })
-  .catch(console.error);
+export * from "./src";
